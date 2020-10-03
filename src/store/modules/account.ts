477a2a32f0d4519f4b9c7899c8597da6968b3d00
Timur Badretdinov <destinerx@gmail.com>
2020-10-03 21:36:50 +0300

    Persist web3 provider between sessions

--- src/store/modules/account.ts
+++ src/store/modules/account.ts
@@ -3,6 +3,8 @@ import { ethers } from 'ethers';
 import lock from '../../utils/connectors';
 import config from '../../config/index';
 
+const LS_CONNECTOR_KEY = '_lock.connector';
+
 const mutations = {
     setWeb3Provider: (_state: any, provider: any): void => {
         _state.web3Provider = provider;
@@ -19,25 +21,47 @@ const mutations = {
 };
 
 const actions = {
-    init: async({ commit }: any): Promise<void> => {
+    init: async({ commit, dispatch }: any): Promise<void> => {
         // Save Alchemy provider as fallback
         const fallbackProvider = new ethers.providers.JsonRpcProvider(config.alchemyUrl);
         commit('setFallbackProvider', fallbackProvider);
-        // TODO Save Web3 provider if available
+        // Save Web3 provider if available
+        const connectorKey = localStorage.getItem(LS_CONNECTOR_KEY);
+        if (connectorKey) {
+            const connector = lock.getConnector(connectorKey);
+            const provider = await connector.connect();
+            dispatch('saveWeb3Provider', provider);
+        }
     },
-    connect: async({ commit }: any, connectorKey: any): Promise<void> => {
+    connect: async({ dispatch }: any, connectorKey: string): Promise<void> => {
         const connector = lock.getConnector(connectorKey);
         const provider = await connector.connect();
+        if (provider) {
+            localStorage.setItem(LS_CONNECTOR_KEY, connectorKey);
+            dispatch('saveWeb3Provider', provider);
+        }
+    },
+    disconnect: async({ commit }: any): Promise<void> => {
+        const connectorKey = localStorage.getItem(LS_CONNECTOR_KEY);
+        if (connectorKey) {
+            const connector = lock.getConnector(connectorKey);
+            const isLoggedIn = await connector.isLoggedIn();
+            if (isLoggedIn) {
+                await connector.logout();
+            }
+            localStorage.removeItem(LS_CONNECTOR_KEY);
+        }
+        commit('setWeb3Provider', null);
+        commit('setAddress', '');
+        commit('setChainId', 0);
+    },
+    saveWeb3Provider: async({ commit }: any, provider: any): Promise<void> => {
         const web3Provider = new ethers.providers.Web3Provider(provider);
         const network = await web3Provider.getNetwork();
         const accounts = await web3Provider.listAccounts();
         commit('setWeb3Provider', web3Provider);
         commit('setAddress', accounts[0]);
         commit('setChainId', network.chainId);
-    },
-    disconnect: async({ commit }: any): Promise<void> => {
-        commit('setWeb3Provider', null);
-        commit('setAddress', '');
     },
 };
 
