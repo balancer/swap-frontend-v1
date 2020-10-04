897c210c4eb384c4558b333a01beb099c3bfa9d6
Timur Badretdinov <destinerx@gmail.com>
2020-10-04 21:13:45 +0300

    Add formatAddress to helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,10 @@
 import BigNumber from 'bignumber.js';
 
+export function formatAddress(address: string, length = 8): string {
+    const ellipsizedAddress = `${address.substr(0, 2 + length / 2)}…${address.substr(42 - length / 2)}`;
+    return ellipsizedAddress;
+}
+
 export function scale(input: BigNumber, decimalPlaces: number): BigNumber {
     const scalePow = new BigNumber(decimalPlaces.toString());
     const scaleMul = new BigNumber(10).pow(scalePow);
