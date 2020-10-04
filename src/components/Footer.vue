d6d3db27fa00e5f981460b205a30f9653ebb1815
Timur Badretdinov <destinerx@gmail.com>
2020-10-04 21:30:54 +0300

    Add Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="footer">
+        <div class="links">
+            <a
+                href="https://pools.balancer.exchange/"
+                target="_blank"
+            >
+                Pool Management
+            </a>
+            <a
+                href="https://vote.balancer.finance/"
+                target="_blank"
+            >
+                Governance
+            </a>
+        </div>
+        <div class="links">
+            <a
+                href="https://discord.com/invite/ARJWaeF"
+                target="_blank"
+            >
+                Discord
+            </a>
+            <a
+                href="https://docs.balancer.finance/"
+                target="_blank"
+            >
+                Docs
+            </a>
+            <a
+                href="https://help.balancer.finance/en/"
+                target="_blank"
+            >
+                Help
+            </a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.footer {
+    height: 64px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.links {
+    margin: 16px;
+}
+
+a {
+    margin-left: 8px;
+    text-decoration: none;
+    color: var(--text-primary);
+}
+</style>
