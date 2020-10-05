42de1525d0c7bb640d6d4e191b0028173cad2b67
Timur Badretdinov <destinerx@gmail.com>
2020-10-05 18:16:47 +0300

    Add ButtonText component

---
+++ src/components/ButtonText.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="button">
+        {{ text }}
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+    },
+});
+</script> 
+
+<style scoped>
+.button {
+    color: var(--accent);
+    text-decoration: underline;
+    cursor: pointer;
+    font-size: 14px;
+}
+</style>
