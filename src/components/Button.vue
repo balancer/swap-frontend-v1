8aa0d880bf6ed5f42ff3d296253ddbfd118faa89
Timur Badretdinov <destinerx@gmail.com>
2020-10-05 11:36:25 +0300

    Add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+    <button>
+        {{ text }}
+    </button>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+    },
+});
+</script>
+
+<style scoped>
+button {
+    height: var(--block-height);
+    width: 160px;
+    font-size: 16px;
+    background: var(--accent);
+    color: var(--text-primary);
+    border: none;
+    border-radius: 4px;
+}
+</style>
