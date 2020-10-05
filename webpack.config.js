0ee5c4c036a825be48f43f09a5ac87ce29c51710
Timur Badretdinov <destinerx@gmail.com>
2020-10-05 14:05:24 +0300

    Reindent Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,78 +7,78 @@ const isDev = process.argv.some(v => v.includes('webpack-dev-server'));
 
 module.exports =
 {
-	// This is the "main" file which should include all other modules
-	entry: './src/main.ts',
-	// Where should the compiled file go?
-	output:
-	{
-		publicPath: '/',
-		filename: '[name].[contenthash].js'
-	},
-	mode: isDev ? 'development' : 'production',
-	devtool: isDev ? '': 'eval',
-	resolve: {
-		alias: {
-			// '@': path.resolve(__dirname, 'src'),
-			vue: '@vue/runtime-dom',
-		},
-		extensions: ['.ts', '.js', '.json'],
-	},
-	module:
-	{
-		rules:
-		[
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-			},
-			{
-				test: /\.tsx?$/,
-				loader: 'ts-loader',
-				// exclude: /node_modules/,
-				options: {
-					appendTsSuffixTo: [/\.vue$/],
-				},
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|jpg|gif|svg)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options:
-						{
-							name: '[path][name].[ext]',
-						},
-					}
-				],
-			},
-		],
-	},
-	plugins:
-	[
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			template: 'public/index.html',
-			favicon: 'public/favicon.ico',
-		}),
-		new VueLoaderPlugin(),
-	],
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendors',
-					chunks: 'all',
-				},
-			},
-		},
-	},
-	devServer: {
-		historyApiFallback: true,
-	},
+    // This is the "main" file which should include all other modules
+    entry: './src/main.ts',
+    // Where should the compiled file go?
+    output:
+    {
+        publicPath: '/',
+        filename: '[name].[contenthash].js',
+    },
+    mode: isDev ? 'development' : 'production',
+    devtool: isDev ? '': 'eval',
+    resolve: {
+        alias: {
+            // '@': path.resolve(__dirname, 'src'),
+            vue: '@vue/runtime-dom',
+        },
+        extensions: ['.ts', '.js', '.json'],
+    },
+    module:
+    {
+        rules:
+        [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                // exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            name: '[path][name].[ext]',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins:
+    [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            favicon: 'public/favicon.ico',
+        }),
+        new VueLoaderPlugin(),
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
 };
