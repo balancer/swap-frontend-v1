d15f1e9fe90bdc9ef5aeaa8eff047333944d374b
Timur Badretdinov <destinerx@gmail.com>
2020-10-06 18:44:10 +0300

    Add isAddress to helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,18 @@
 import BigNumber from 'bignumber.js';
+import { ethers } from 'ethers';
 
 export function formatAddress(address: string, length = 8): string {
     const ellipsizedAddress = `${address.substr(0, 2 + length / 2)}…${address.substr(42 - length / 2)}`;
     return ellipsizedAddress;
+}
+
+export function isAddress(value: string): boolean {
+    try {
+        ethers.utils.getAddress(value);
+    } catch(e) {
+        return false;
+    }
+    return true;
 }
 
 export function scale(input: BigNumber, decimalPlaces: number): BigNumber {
