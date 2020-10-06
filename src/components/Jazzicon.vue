4bd3650d83f10a75c536d0878844a884887e7cad
Timur Badretdinov <destinerx@gmail.com>
2020-10-06 17:13:21 +0300

    Add Jazzicon component

---
+++ src/components/Jazzicon.vue
@@ -0,0 +1,41 @@
+<template>
+    <div ref="icon" />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted, watch } from 'vue';
+// @ts-ignore
+import jazzicon from '@metamask/jazzicon';
+
+export default defineComponent({
+    props: {
+        address: {
+            type: String,
+            required: true,
+        },
+    },
+    setup(props) {
+        const icon = ref(null);
+
+        onMounted(() => {
+            update();
+        });
+
+        watch(props, () => {
+            update();
+        });
+
+        function update(): void {
+            const address = props.address;
+            // @ts-ignore
+            icon.value.innerHTML = '';
+            // @ts-ignore
+            icon.value.appendChild(jazzicon(16, parseInt(address.slice(2, 10), 16)));
+        }
+
+        return {
+            icon,
+        };
+    },
+});
+</script>
