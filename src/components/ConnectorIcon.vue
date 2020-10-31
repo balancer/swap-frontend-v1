db33c9b5f304273bbcb6177350f8a42a37b73363
Timur Badretdinov <destinerx@gmail.com>
2020-10-31 11:25:20 +0300

    Add ConnectorIcon component

---
+++ src/components/ConnectorIcon.vue
@@ -0,0 +1,25 @@
+<template>
+    <img :src="icon">
+</template>
+
+<script>
+import { defineComponent, computed } from 'vue';
+
+export default defineComponent({
+    props: {
+        connector: {
+            type: String,
+            required: true,
+        },
+    },
+    setup(props) {
+        const icon = computed(() => {
+            return `https://raw.githubusercontent.com/snapshot-labs/lock/master/connectors/assets/${props.connector}.png`;
+        });
+
+        return {
+            icon,
+        };
+    },
+});
+</script>
