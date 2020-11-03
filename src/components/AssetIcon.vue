8cc5843bdc66b0a83c7085bb1421b76b97bc4c14
Timur Badretdinov <destinerx@gmail.com>
2020-11-03 11:39:16 +0300

    Handle "logo not found" for custom assets

--- src/components/AssetIcon.vue
+++ src/components/AssetIcon.vue
@@ -1,9 +1,12 @@
 <template>
-    <img :src="assetIcon">
+    <img
+        :src="assetIcon"
+        @error="handleError"
+    >
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue';
+import { defineComponent, computed, ref, toRefs, watch } from 'vue';
 import { useStore } from 'vuex';
 
 import defaultIcon from '@/assets/defaultAssetIcon.svg';
@@ -22,6 +25,14 @@ export default defineComponent({
     setup(props) {
         const store = useStore<RootState>();
 
+        const loadingFailed = ref(false);
+
+        const { address } = toRefs(props);
+
+        watch(address, () => {
+            loadingFailed.value = false;
+        });
+
         const assetIcon = computed(() => {
             let address = props.address;
             const { metadata } = store.state.assets;
@@ -33,11 +44,19 @@ export default defineComponent({
             if (!assetMetadata) {
                 return defaultIcon;
             }
+            if (loadingFailed.value) {
+                return defaultIcon;
+            }
             return assetMetadata.logoUrl;
         });
 
+        function handleError(): void {
+            loadingFailed.value = true;
+        }
+
         return {
             assetIcon,
+            handleError,
         };
     },
 });
