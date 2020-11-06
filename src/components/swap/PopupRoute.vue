74bebfa978c1398cde4fbb527ebcd7116a27dc43
Timur Badretdinov <destinerx@gmail.com>
2020-11-06 12:48:16 +0300

    Show swap asset distribution

--- src/components/swap/PopupRoute.vue
+++ src/components/swap/PopupRoute.vue
@@ -11,25 +11,23 @@
                 Multihop swap ({{ routeCount }} routes, {{ hopCount }} hops)
             </div>
             <div
-                v-for="(route, index) in swaps"
+                v-for="(route, index) in routes"
                 :key="index"
                 class="route"
             >
                 <div class="route-share">
-                    {{ getShare(route) }}%
+                    {{ formatShare(route.share) }}
                 </div>
                 <div
-                    v-for="hop in route"
-                    :key="hop"
-                    class="hop"
+                    v-for="asset in route.assets"
+                    :key="asset.address"
+                    class="asset"
                 >
-                    <a
-                        :href="getPoolLink(hop.pool)"
-                        target="_blank"
-                        class="pool-link"
-                    >
-                        {{ formatAddress(hop.pool) }}
-                    </a>
+                    <AssetIcon
+                        :address="asset.address"
+                        class="asset-icon"
+                    />
+                    {{ asset.symbol }}
                 </div>
             </div>
         </div>
@@ -37,21 +35,31 @@
 </template>
 
 <script lang="ts">
+import { BigNumber } from 'bignumber.js';
 import { defineComponent, computed } from 'vue';
+import { useStore } from 'vuex';
+import { getAddress } from '@ethersproject/address';
 import { Swap } from '@balancer-labs/sor/dist/types';
 
-import { formatAddress, getPoolLink } from '@/utils/helpers';
+import { formatAddress } from '@/utils/helpers';
+import { RootState } from '@/store';
 
+import AssetIcon from '@/components/AssetIcon.vue';
 import Icon from '@/components/Icon.vue';
-import { BigNumber } from 'bignumber.js';
+
+interface Asset {
+    symbol: string;
+    address: string;
+}
 
 interface Route {
-    distribution: number;
-    tokens: string[];
+    share: number;
+    assets: Asset[];
 }
 
 export default defineComponent({
     components: {
+        AssetIcon,
         Icon,
     },
     props: {
@@ -61,6 +69,8 @@ export default defineComponent({
         },
     },
     setup(props) {
+        const store = useStore<RootState>();
+
         const routeCount = computed(() => props.swaps.length);
 
         const hopCount = computed(() => {
@@ -70,24 +80,60 @@ export default defineComponent({
             }, 0);
         });
 
-        const totalSwapAmount = computed(() => {
+        const routes = computed(() => {
             const swaps = props.swaps as Swap[][];
-            return swaps.reduce((swapAmount, route) => {
+            const { metadata } = store.state.assets;
+
+            const totalSwapAmount = swaps.reduce((swapAmount, route) => {
                 return swapAmount.plus(route[0].swapAmount || '0');
             }, new BigNumber(0));
+
+            return swaps.map(route => {
+                const assets: Asset[] = [];
+                for (let i = 0; i < route.length; i++) {
+                    const hop = route[i];
+                    if (i === 0) {
+                        const { tokenIn } = hop;
+                        const address = getAddress(tokenIn);
+                        const tokenInMetadata = metadata[address];
+                        const symbol = tokenInMetadata
+                            ? tokenInMetadata.symbol
+                            : formatAddress(address, 4);
+                        assets.push({
+                            address,
+                            symbol,
+                        });
+                    }
+                    const { tokenOut } = hop;
+                    const address = getAddress(tokenOut);
+                    const tokenOutMetadata = metadata[address];
+                    const symbol = tokenOutMetadata
+                        ? tokenOutMetadata.symbol
+                        : formatAddress(address, 4);
+                    assets.push({
+                        address,
+                        symbol,
+                    });
+                }
+                const swapAmount = new BigNumber(route[0].swapAmount || '0');
+                const share = swapAmount.div(totalSwapAmount);
+                return {
+                    assets,
+                    share,
+                };
+            });
         });
 
-        function getShare(route: Swap[]): string {
-            const swapAmount = new BigNumber(route[0].swapAmount || '0');
-            return swapAmount.div(totalSwapAmount.value).times(100).toFixed(0, BigNumber.ROUND_HALF_UP);
+        function formatShare(share: BigNumber): string {
+            return `${share.times(100).toFixed(0, BigNumber.ROUND_HALF_UP)}%`;
         }
 
         return {
             routeCount,
             hopCount,
-            getShare,
+            routes,
+            formatShare,
             formatAddress,
-            getPoolLink,
         };
     },
 });
@@ -108,18 +154,18 @@ export default defineComponent({
 }
 
 .popup {
-    width: 240px;
+    width: 280px;
     z-index: 1;
     bottom: 100%;
     left: 50%;
-    margin-left: -129px;
+    margin-left: -149px;
     margin-bottom: 8px;
     padding: 8px;
     position: absolute;
     display: flex;
     flex-direction: column;
     visibility: hidden;
-    color: var(--text-secondary);
+    color: var(--text-primary);
     border: 1px solid var(--outline);
     background-color: var(--background-secondary);
     border-radius: var(--border-radius);
@@ -153,6 +199,7 @@ export default defineComponent({
 
 .header {
     margin-bottom: 16px;
+    color: var(--text-secondary);
     font-size: 14px;
     text-align: center;
 }
@@ -173,12 +220,19 @@ export default defineComponent({
     width: 40px;
 }
 
-.hop:not(:last-child)::after {
+.asset {
+    display: flex;
+}
+
+.asset:not(:last-child)::after {
     content: '⭢';
     margin: 0 4px;
 }
 
-.pool-link {
-    color: var(--text-primary);
+.asset-icon {
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    margin-right: 4px;
 }
 </style>
