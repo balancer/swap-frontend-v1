335838e1e2f8fedbfdc4acb5a25f0d1d7c574694
Timur Badretdinov <destinerx@gmail.com>
2020-11-11 15:41:28 +0300

    Fetch asset prices

--- src/store/modules/assets.ts
+++ src/store/modules/assets.ts
@@ -1,3 +1,4 @@
+import Coingecko from '@/api/coingecko';
 import Ethereum from '@/api/ethereum';
 import { RootState } from '@/store';
 import config, { TokenMetadata } from '@/config';
@@ -5,30 +6,42 @@ import { ActionContext } from 'vuex';
 
 export interface AssetState {
     metadata: Record<string, TokenMetadata>;
+    price: Record<string, number>;
 }
 
 const mutations = {
     addMetadata: (_state: AssetState, metadata: Record<string, TokenMetadata>): void => {
-        for (const assetsAddress in metadata) {
-            _state.metadata[assetsAddress] = metadata[assetsAddress];
+        for (const address in metadata) {
+            _state.metadata[address] = metadata[address];
+        }
+    },
+    addPrice: (_state: AssetState, price: Record<string, number>): void => {
+        for (const address in price) {
+            _state.price[address] = price[address];
         }
     },
 };
 
 const actions = {
-    init: ({ commit }: ActionContext<AssetState, RootState>): void => {
+    init: async({ commit }: ActionContext<AssetState, RootState>): Promise<void> => {
         const metadata = config.tokens;
         commit('addMetadata', metadata);
+        const assets = Object.keys(config.tokens);
+        const price = await Coingecko.fetchPrice(assets);
+        commit('addPrice', price);
     },
     fetch: async({ commit }: ActionContext<AssetState, RootState>, assets: string[]): Promise<void> => {
         const metadata = await Ethereum.fetchTokenMetadata(assets);
         commit('addMetadata', metadata);
+        const price = await Coingecko.fetchPrice(assets);
+        commit('addPrice', price);
     },
 };
 
 function state(): AssetState {
     return {
         metadata: {},
+        price: {},
     };
 }
 
