ebb7536709ad906074d5b5fa1a51561439d2a8ec
Timur Badretdinov <destinerx@gmail.com>
2020-11-27 01:46:42 +0300

    Button styling

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -41,30 +41,30 @@ button {
     align-items: center;
     justify-content: center;
     font-size: var(--font-size-large);
-    background: var(--background-secondary);
+    font-weight: bold;
     color: var(--text-primary);
+    background: linear-gradient(190deg, #f0f -110%, #00f 100%);
+    box-shadow: 0 5px 5px rgba(0, 0, 0, 0.1);
     border-radius: var(--border-radius-medium);
     border-color: var(--background-secondary);
     cursor: pointer;
-    transition: background 0.25s ease-in, border-color 0.25s ease-in;
 }
 
 button.primary {
-    background: var(--accent);
+    background: linear-gradient(190deg, #f0f -110%, #00f 100%);
 }
 
 button:hover {
-    background: var(--background-primary);
+    background: linear-gradient(190deg, #f0f -80%, #00f 100%);
 }
 
 button.primary:hover {
-    background: var(--accent-dark);
+    background: linear-gradient(190deg, #f0f -80%, #00f 100%);
 }
 
 button:disabled,
 button:disabled:hover {
     background: var(--text-secondary);
     cursor: not-allowed;
-    transition: background 0.35s ease-out, border-color 0.35s ease-out;
 }
 </style>
