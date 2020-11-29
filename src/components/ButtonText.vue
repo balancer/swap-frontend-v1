c835984175b9148a011d92b0e75ce05f1716d577
Timur Badretdinov <destinerx@gmail.com>
2020-11-29 17:53:28 +0300

    ButtonText hover

--- src/components/ButtonText.vue
+++ src/components/ButtonText.vue
@@ -23,8 +23,13 @@ export default defineComponent({
     color: var(--text-control);
     font-size: var(--font-size-tiny);
     text-transform: capitalize;
+    border: 1px solid var(--border-input);
     border-radius: var(--border-radius-small);
     background: var(--background-control);
     cursor: pointer;
+}
+
+.button:hover {
+    background: var(--background-hover);
 }
 </style>
