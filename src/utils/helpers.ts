8dd1d55ebc08c5481f34902a49f6580ab3eaaa68
rabmarut <67169632+rabmarut@users.noreply.github.com>
2021-02-02 18:48:45 -0500

    Include xHDX logo manually
    xHDX has an upcoming LBP on Feb 8th.

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -90,6 +90,9 @@ export function getAssetLogo(address: string): string {
     if (address.toLowerCase() === '0x50de6856358cc35f3a9a57eaaa34bd4cb707d2cd') {
         return 'https://raw.githubusercontent.com/balancer-labs/assets/master/assets/0x50de6856358cc35f3a9a57eaaa34bd4cb707d2cd.png';
     }
+    if (address.toLowerCase() === '0x6fcb6408499a7c0f242e32d77eb51ffa1dd28a7e') {
+        return 'https://raw.githubusercontent.com/balancer-labs/assets/master/assets/0x6fcb6408499a7c0f242e32d77eb51ffa1dd28a7e.png';
+    }
     if (address.toLowerCase() === '0xffffffff2ba8f66d4e51811c5190992176930278') {
         return 'https://raw.githubusercontent.com/balancer-labs/assets/master/assets/0xffffffff2ba8f66d4e51811c5190992176930278.png';
     }
