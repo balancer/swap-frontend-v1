c456f9abcd56d0ba36cde1aa2a6f32d70f7efbd0
bonustrack <fabien@bonustrack.co>
2021-02-17 15:34:30 +0700

    Enable only for mainnet

--- src/utils/fathom.ts
+++ src/utils/fathom.ts
@@ -8,11 +8,11 @@ const actionToGoalId = {
 
 export function setGoal(action: string, value = 0): void {
     const id = actionToGoalId[action];
-    if (window['fathom'] && config.chainId === 42 && id)
+    if (window['fathom'] && config.chainId === 1 && id)
         window['fathom'].trackGoal(id, value);
 }
 
-if (config.chainId === 42) {
+if (config.chainId === 1) {
     const script = document.createElement('script');
     script.setAttribute('src', 'https://cdn.usefathom.com/script.js');
     script.setAttribute('data-spa', 'auto');
