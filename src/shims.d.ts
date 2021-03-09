9fd90841f823e89eea0e1030aabc1a4f6960fa45
Timur Badretdinov <destinerx@gmail.com>
2021-03-09 18:06:19 +0300

    Remove png from shims

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -9,11 +9,6 @@ declare module '*.svg' {
     export default content;
 }
 
-declare module '*.png' {
-    const content: any;
-    export default content;
-}
-
 interface Window {
     ethereum: any;
 }
