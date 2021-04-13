fdcf23ee8d11dda036edaf724c63e800a0941d50
Alex O <maightycron@gmail.com>
2021-04-13 16:09:59 +0500

    WIP

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
+
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
