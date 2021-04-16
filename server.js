7b4e1c0309571a8ac46965662d1642a38be1551a
Alex O <maightycron@gmail.com>
2021-04-16 15:55:23 +0500

    CORS

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
+app.all('*', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+});
 
 
 var server = app.listen(app.get('port'), function() {
