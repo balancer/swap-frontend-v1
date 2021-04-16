b011b2eacc2c108381bc02253b2d0075ae449513
Alex O <maightycron@gmail.com>
2021-04-16 15:19:08 +0500

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -73,6 +73,7 @@ module.exports =
         new HtmlWebpackPlugin({
             template: 'public/index.html',
             favicon: 'public/favicon.ico',
+            favicon: 'public/pools-data.json',
         }),
         new VueLoaderPlugin(),
     ],
