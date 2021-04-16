60fb94cb672ff35b2e7e7dfde4bfe3b522367569
Alex O <maightycron@gmail.com>
2021-04-16 16:10:52 +0500

    CORS

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ var express = require('express');
 
 var app = express();
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Expose-Headers', 'Content-Length');
+    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+    if (req.method === 'OPTIONS') {
+      return res.send(200);
+    } else {
+      return next();
+    }
+});
+
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
-app.all('*', function (req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-});
 
 
 var server = app.listen(app.get('port'), function() {
