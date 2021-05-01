d5d7f7dae3d889efeb22aad9f2d356f14985ea18
Timur Badretdinov <destinerx@gmail.com>
2021-05-01 13:09:41 +0300

    Remove reverted tx logging

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -98,17 +98,3 @@ export function getAssetLogo(address: string): string {
     }
     return `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`;
 }
-
-export function logRevertedTx(
-    sender: string,
-    contract: Contract,
-    action: string,
-    params: any,
-    overrides: any,
-): void {
-    overrides.gasPrice = sender;
-    const dummyPrivateKey = '0x651bd555534625dc2fd85e13369dc61547b2e3f2cfc8b98cee868b449c17a4d6';
-    const dummyWallet = new Wallet(dummyPrivateKey).connect(debugProvider);
-    const loggingContract = contract.connect(dummyWallet);
-    loggingContract[action](...params, overrides);
-}
